1f03aed49551b3bdb2745f6c08891583243da1b2
Baboa123 <97915531+Baboa123@users.noreply.github.com>
2024-06-03 15:28:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 document.getElementById('save-button').addEventListener('click', function () {
-    const name = document.getElementById('name').value;
-    const password = document.getElementById('password').value;
+    // Získanie údajov z polí pre e-mail/užívateľské meno a heslo
+    const emailOrUsername = document.getElementById('login-email').value;
+    const password = document.getElementById('login-password').value;
 
     // Vytvorenie JSON objektu s údajmi
     const userData = {
-        name: name,
+        emailOrUsername: emailOrUsername,
         password: password
     };
 
     // Konverzia JSON objektu na reťazec
     const userDataString = JSON.stringify(userData);
 
-    // Vytvorenie súboru s údajmi
+    // Vytvorenie a uloženie súboru s údajmi pomocou HTML5 File API
     const blob = new Blob([userDataString], { type: 'application/json' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'user_data.json';
-    a.click();
-    URL.revokeObjectURL(url);
-
-    // Vyčistenie formulára
-    document.getElementById('save-form').reset();
+    const fileName = 'user_data.json';
+    if (window.navigator.msSaveOrOpenBlob) {
+        // Internet Explorer
+        window.navigator.msSaveOrOpenBlob(blob, fileName);
+    } else {
+        // Iné prehliadače
+        const a = document.createElement('a');
+        const url = URL.createObjectURL(blob);
+        a.href = url;
+        a.download = fileName;
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+        document.body.removeChild(a);
+    }
 });
+
